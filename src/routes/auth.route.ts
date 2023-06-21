f24bb23778d08f11cead3e3f698683d9a6872c01
Caio Domingos <caiod.dev@gmail.com>
2023-06-21 16:52:42 -0300

    fix login

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -16,7 +16,7 @@ class AuthRoute implements Routes {
 
   private initializeRoutes() {
     this.router.post(`${this.path}signup`, validationMiddleware(UsuarioCreateDTO, 'body'), this.authController.signUp);
-    this.router.post(`${this.path}login`, validationMiddleware(UsuarioLoginDTO, 'body'), this.authController.logInWithKey);
+    this.router.post(`${this.path}login`, validationMiddleware(UsuarioLoginDTO, 'body'), this.authController.logIn);
     this.router.post(`${this.path}login/key`, this.authController.logInWithKey);
     this.router.post(`${this.path}verifyToken`, this.authController.verifyToken);
     this.router.post(`${this.path}forgotPassword`, this.authController.forgotPassword);
